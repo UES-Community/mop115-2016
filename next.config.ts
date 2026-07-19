c77f1247fd728310379441dab56018b49211e9d1
Cristian Antonio Escalante Hernandez <heac.creative@gmail.com>
2026-07-19 12:32:47 -0600

    ci(deploy): actualizar workflow a Node 22 y agregar basePath dinamico para GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const repo = process.env.GITHUB_REPOSITORY
+  ? process.env.GITHUB_REPOSITORY.replace(/.*?\//, "")
+  : "";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: isGithubActions && repo ? `/${repo}` : "",
   images: {
     unoptimized: true,
   },
